75a07e2a638718c500082211d1e3d159942bf54b
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-03-22 02:21:12 +0100

    Add initial components

---
+++ src/pages/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>
+             <ul>
+                <li>Analytics</li>
+                <li>Profile</li>
+                <li>Settings</li>
+             </ul>
+        </div>
+    )
+}
+export default Dashboard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Dashboard from "./dashboard/dashboard";
+import Dashboard from "./dashboard";
+import Header from "./components/Header";
+import SideMenu from "./components/SideMenu";
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
+      <Header />
+      <SideMenu />
       <Dashboard />
     </main>
   );
